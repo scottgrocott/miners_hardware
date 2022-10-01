79c98925ca9e35a42267abb24393ddff8a06f80a
Scott Grocott <sagrocott@protonmail.com>
2022-10-01 14:56:12 -0700

    added mongo

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,11 @@
 <!--beginning of footer-->
     <script src="assets/dist/js/bootstrap.bundle.min.js"></script>  </div>
 </div>
-
+<!--
  <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script>
-	  <script src="/assets/dashboard.js"></script>
+<script src="/assets/dashboard.js"></script>
+-->	 
+	 
 
     
 
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+require_once __DIR__ . '/vendor/autoload.php';
 switch ($_SERVER['REQUEST_URI']) {
     case '/dashboard':
         include 'pages/dashboard.php';
